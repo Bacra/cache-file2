17e47d89a7ccf3166d48c799477460ae6eab7d47
Bacra Woo <bacra.woo@gmail.com>
2015-08-02 13:43:58 +0800

    Update: assert in mocha

--- test/test-index.js
+++ test/test-index.js
@@ -1,24 +1,95 @@
-require('debug').enable('safe-write*');
+// require('debug').enable('safe-write*');
 
 var assert = require('assert');
 var safeWrite = require('../.');
+var fs = require('fs');
+var rmdir = require('rmdir');
+var testPath = __dirname+'/tmp';
 
-var writer = safeWrite.Writer(__dirname+'/tmp/cahce');
-writer.add('file1.json', function(err, addContent) {
-	assert.ok(!err, 'lock err');
-	addContent('content', function(err) {
-		assert.ok(!err, 'add content err');
+
+describe('[Async Base]', function() {
+	before(function(done) {
+		if (!fs.existsSync(testPath)) return done();
+		rmdir(testPath, done);
 	});
+
+	var testfile = 'file1.json';
+
+	function testAsync(name, filename, exthandler) {
+		if (!filename) filename = name+'.json';
+		if (!exthandler) exthandler = function(){};
+		var file = testPath+'/'+filename;
+
+		it(name, function(done) {
+			var writer = safeWrite.Writer(testPath);
+			assert.ok(!fs.existsSync(file));
+
+			var asyncTask = 2;
+			function extasync() {
+				asyncTask++;
+				return relDone;
+			}
+			function relDone() {
+				if (!--asyncTask) done();
+			}
+
+			var extParams = {
+				name: name,
+				filename: filename,
+				file: file,
+				writer: writer,
+				async: extasync
+			};
+
+			writer.add(filename, function(err, addContent) {
+				assert.ok(!err, 'init add lock err');
+				var fileContent = 'write some thing';
+
+				exthandler('lock', extParams);
+
+				addContent(fileContent, function(err) {
+					assert.ok(!err, 'add content err');
+					console.log(filename, fs.existsSync(file));
+					exthandler('writeEnd', extParams);
+					relDone();
+				});
+
+				writer.read(filename, function(err, content) {
+					assert.ok(err);
+					relDone();
+				});
+			});
+
+			assert.ok(!fs.existsSync(file));
+
+			try {
+				assert.ok(!writer.readSync(filename).toString());
+			} catch(err) {
+				assert.ok(err);
+			}
+
+			exthandler('unlock', extParams);
+		});
+	}
+
+	testAsync('base');
+	testAsync('addSync', null, function(flow, params) {
+		function doWrite() {
+			var newfileContent = 'new content:'+flow;
+			params.writer.addSync(params.filename, newfileContent);
+			assert.ok(fs.existsSync(params.file));
+			assert.equal(fs.readFileSync(params.file).toString(), newfileContent);
+		}
+
+		if (flow == 'writeEnd') {
+			doWrite();
+		} else if (flow == 'lock') {
+			assert.throws(doWrite);
+		}
+	});
+
 });
 
-writer.read('file1.json', function(err) {
-	assert.ok(!!err, 'not lock');
-});
 
-assert.throws(function() {
-	writer.readSync('file1.json');
-}, 'not lock');
 
-assert.throws(function() {
-	writer.addSync('file1.json', 'content2');
-}, 'not lock');
+
