7c2412c5a345abbfd37a899b495e4970af725d6f
bacrawu <bacrawu@tencent.com>
2021-03-13 02:16:17 +0800

    clear code

--- README.md
+++ README.md
@@ -6,33 +6,6 @@ Store and get files width file locker. It is safe in mulit process.
 ## Usage
 
 ```javascript
-var safeWrite = require('safe-write')
-
-safeWrite.write('./file.json', 'newContent', 'oldContent', function(err) {});
-
-safeWrite.read('./file2.json', function(err, content) {});
+const safeWrite = require('safe-write')
+safeWrite('./file.json', 'newContent').then(() => {});
 ```
-
-## Methods
-
-### safeWrite.write(file, newContent[, oldContent][, callback][, ignoreUnlockErr])
-
-write file width file lock.
-
-return Promise Object.
-
-### safeWrite.fastWrite(file, newContent[, oldContent][, callback])
-
-wirte file widthout file lock.
-
-return Promise Object.
-
-### safeWrite.read(file[, callback][, ignoreUnlockErr])
-
-read file with file lock.
-
-return Promise Object.
-
-## Options
-
-See [lockfile](https://github.com/npm/lockfile#options)
--- test/test-index.js
+++ test/test-index.js
@@ -1,128 +1,84 @@
-var debug		= require('debug');
-debug.enable('safe-write:test');
-// debug.enable('*');
+const debug = require('debug')('safe-write:test');
+require('debug').enable('safe-write:test');
 
-var testlog		= debug('safe-write:test');
-var cluster		= require('cluster');
-var testPath	= __dirname+'/tmp';
-var testFileNum	= 3;
-var clientNum	= 5;
-var writeTimes	= 100;
+const fs = require('fs');
+const cluster = require('cluster');
+const testPath = __dirname + '/tmp';
+const testFileNum = 3;
+const clientNum = 5;
+const writeTimes = 100;
 
 process.env.UV_THREADPOOL_SIZE = 64;
 
-
-if (cluster.isMaster)
-{
-	var fs		= require('fs');
-	var assert	= require('assert');
+if (cluster.isMaster) {
+	const assert = require('assert');
 
 	fs.existsSync(testPath) ? require('rmdir')(testPath, done) : done();
 
-	function done()
-	{
-		var startProNum		= clientNum;
-		var waitExitNum		= clientNum;
-		var waitOnlineNum	= clientNum;
-		var workers = [];
+	function done() {
+		let startProNum = clientNum;
+		let waitExitNum = clientNum;
+		let waitOnlineNum = clientNum;
+		const workers = [];
 
-		while(startProNum--)
-		{
-			var f = cluster.fork();
-			f.once('online', function()
-				{
-					if (!--waitOnlineNum)
-					{
-						require('mkdirp').sync(testPath);
-						eachFile(function(file, fileIndex)
-						{
-							fs.closeSync(fs.openSync(file, 'a'));
+		while (startProNum--) {
+			const f = cluster.fork();
+			f.once('online', () => {
+				if (!--waitOnlineNum) {
+					require('mkdirp').sync(testPath);
+					mapFile(file => fs.closeSync(fs.openSync(file, 'a')));
+
+					workers.forEach(f => f.send('start'));
+				}
+			})
+				.once('exit', () => {
+					if (!--waitExitNum) {
+						const expectLineNum = writeTimes * clientNum;
+						mapFile((file, fileIndex) => {
+							assert.ok(fs.existsSync(file), 'content ' + fileIndex + ' err');
+							const fileLine = fs.readFileSync(file).toString().split('\n').length;
+							debug('file%d: %d/%d', fileIndex, fileLine, expectLineNum);
+							assert.ok(fileLine <= expectLineNum, 'write ' + fileIndex + ' line:' + fileLine + '/' + expectLineNum);
 						});
 
-						workers.forEach(function(f)
-						{
-							f.send('start');
-						});
-					}
-				})
-				.once('exit', function()
-				{
-					if (!--waitExitNum)
-					{
-						var expectLineNum = (writeTimes)*clientNum+1;
-						eachFile(function(file, fileIndex)
-						{
-							assert.ok(fs.existsSync(file), 'content '+fileIndex+' err');
-							var fileLine = fs.readFileSync(file).toString().split('\n').length;
-							testlog('fileline %d:%d/%d', fileIndex, fileLine, expectLineNum);
-							assert.ok(fileLine <= expectLineNum, 'write '+fileIndex+' line:'+fileLine+'/'+expectLineNum);
-						});
-						// process.exit();
+						process.exit();
 					}
 				});
 
 			workers.push(f);
 		}
 	}
-}
-else
-{
-	process.on('message', function(msg)
-	{
+
+} else {
+
+	process.on('message', async (msg) => {
 		if (msg != 'start') return;
 
-		var safeWrite = require('../');
-		var waitFiles = testFileNum;
-		testlog('start write query, pid:%d', process.pid);
+		const safeWrite = require('../');
+		debug('start write query, pid:%d', process.pid);
 
-		eachFile(function(file, fileIndex)
-		{
-			process.nextTick(function()
-			{
-				var baseContent = 'file'+fileIndex+','+process.pid+',';
-				var pro = Promise.resolve();
+		const promises = mapFile(async (file, fileIndex) => {
+			const baseContent = 'file' + fileIndex + ',' + process.pid + ',';
 
-				for(var i = writeTimes; i--;)
-				{
-					pro = pro.then(function()
-							{
-								return safeWrite.read(file);
-							})
-							.then(function(content)
-							{
-								var newContent = baseContent+Date.now();
-
-								if (content) {
-									// testlog('<%d,%d> read rs:%d', process.pid, fileIndex, content.toString().split('\n').length);
-									newContent = content +'\n'+newContent;
-								}
-
-								return safeWrite.write(file, newContent)
-										.catch(function(err)
-										{
-											err && testlog('write err, %o', err);
-										});
-							},
-							function(err)
-							{
-								testlog('<%d,%d> read err:%o', process.pid, fileIndex, err);
-							});
-				}
-
-				pro.then(function()
-				{
-					if (!--waitFiles) process.exit();
-				});
-			});
+			for (let i = writeTimes; i--;) {
+				const newContent = baseContent + Date.now();
+				await safeWrite(file, fs.readFileSync(file) + '\n' + newContent);
+			}
 		});
+
+		await Promise.all(promises);
+		process.exit();
 	});
 }
 
 
-function eachFile(handler)
-{
-	for(var i = testFileNum; i--;)
-	{
-		(handler)(testPath+'/tmp'+i, i);
+function mapFile(handler) {
+	const result = [];
+
+	for (let i = testFileNum; i--;) {
+		const item = handler(testPath + '/tmp' + i, i);
+		result.push(item);
 	}
+
+	return result;
 }
